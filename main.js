bf6c245e3cc01af3e582003680e49b9fa71db9d1
mahe7star <mp1995singh@gmail.com>
2018-10-07 22:58:15 +0530

    something

--- main.js
+++ main.js
@@ -6,23 +6,38 @@
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         // validate on form submit
-        Array.prototype.map.call(inputFields, (input) => {
-            validate(input); 
-        });
+        validate();
     });
 
     // Validate on input change
     Array.prototype.map.call(inputFields, (input) => {
         input.addEventListener('change', (event)=>{
-            validate(event.target);
+            validateInputFields(event.target);
         })
     });
 
-    function validate(input){
-        const alert = document.getElementById(`${input.getAttribute('name')}-alert`);
-        const levelSelect = document.getElementsByTagName('select')[0];
+    function validate(){
+        Array.prototype.map.call(inputFields, (input) => {
+            validateInputFields(input);
+        });
+        validateCheckboxes();
+        validateSelectTag();
+    }
+
+    function validateSelectTag() {
+        const levelSelect = document.getElementsByTagName('select')[0].value;
         const levelAlert = document.getElementById('level-alert');
-        
+        if(levelSelect === ''){
+            levelAlert.classList.remove('in');
+            return true;
+        } else {
+            levelAlert.classList.add('in');
+            return false;
+        }
+    }
+
+    function validateInputFields(input){
+        const alert = document.getElementById(`${input.getAttribute('name')}-alert`);
         const checkName = /^[a-zA-Z0-9]/.test(input.value);
         const checkEmail = /^[a-zA-Z0-9]+@[a-z0-9]+\.[a-z]{2,3}$/.test(input.value);
         const checkMobile = /^[0-9]{10,15}/.test(input.value);
@@ -55,17 +70,10 @@
             }
 
         }
-
-        
-
-        if(input.getAttribute('type') === 'checkbox'){
-            if(input.checked){
-                alert.classList.remove('in');
-                return true;
-            }else {
-                alert.classList.add('in');
-                return false;
-            }
-        }
     }
+
+    function validateCheckboxes(){
+
+    }
+
 })();
